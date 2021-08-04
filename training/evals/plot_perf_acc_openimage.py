020616349faa2e3995a17625fe268e480cc168a8
liecn <lichenni@msu.edu>
2021-08-04 14:03:29 -0400

    prepare for the final

--- training/evals/plot_perf_acc_openimage.py
+++ training/evals/plot_perf_acc_openimage.py
@@ -114,20 +114,20 @@ def main(files):
         walltime[-1] = walltime[-1][:len(metrics[-1])]
         epoch[-1] = epoch[-1][:len(metrics[-1])]
         plot_metric = metrics_label[history['task']]
-    setting_labels[0]='Random+Yogi'
-    setting_labels[1]='Oort+Yogi'
-    setting_labels[2]='Ours+Yogi'
+    # setting_labels[0]='Random+Yogi'
+    # setting_labels[1]='Oort+Yogi'
+    # setting_labels[2]='Ours+Yogi'
     # setting_labels[-2]='centralized+Yogi'
     # setting_labels[-1]='centralized+Prox'
-    plot_line(metrics, walltime, setting_labels, 'Training Epoch', plot_metric, 'time_to_acc_openimage.png')
+    plot_line(metrics, walltime, setting_labels, 'Training Time', plot_metric, 'time_to_acc_openimage.png')
 
 # main(sys.argv[1:])
 # main(['logs/openimage/0713_135941_424/aggregator/training_perf','logs/openimage/0713_135942_52497/aggregator/training_perf','logs/openimage/0713_141018_1562/aggregator/training_perf','logs/openimage/0713_141018_58346/aggregator/training_perf','logs/openimage/0719_210948_48370/aggregator/training_perf','logs/openimage/0719_211249_44960/aggregator/training_perf','logs/openimage/0730_111532_11308/aggregator/training_perf','logs/openimage/0730_205525_50690/aggregator/training_perf'])
 
 # main(['logs/openimage/0713_143547_30464/aggregator/training_perf','logs/openimage/0713_143547_47926/aggregator/training_perf','logs/openimage/0713_201601_8540/aggregator/training_perf','logs/openimage/0714_114408_25538/aggregator/training_perf','logs/openimage/0719_113955_1403/aggregator/training_perf','logs/openimage/0719_114250_5117/aggregator/training_perf'])
 
-# main(['logs/openimage/0719_210948_48370/aggregator/training_perf','logs/openimage/0719_211249_44960/aggregator/training_perf','logs/openimage/0730_164926_29450/aggregator/training_perf','logs/openimage/0730_204956_48363/aggregator/training_perf'])
+# enforce_random
+main(['logs/openimage/0803_212515_51692/aggregator/training_perf','logs/openimage/0803_212614_29422/aggregator/training_perf','logs/openimage/0804_084216_44366/aggregator/training_perf','logs/openimage/0804_084256_23531/aggregator/training_perf'])
 
-main(['logs/openimage/0802_231336_39302/aggregator/training_perf','logs/openimage/0802_230026_27987/aggregator/training_perf','logs/openimage/0802_230255_44225/aggregator/training_perf','logs/openimage/0803_085049_2649/aggregator/training_perf','logs/openimage/0803_085744_1193/aggregator/training_perf','logs/openimage/0803_090655_5323/aggregator/training_perf','logs/openimage/0803_125945_25555/aggregator/training_perf'])
 
 
